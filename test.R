435e4e6a6137b3bcc2ff802253d1c9d486b425fe
Wesley <wesley.nmtorres@gmail.com>
2016-04-21 10:48:07 -0300

    regression by line bus group

--- test.R
+++ test.R
@@ -1,29 +1,39 @@
 library(ggplot2)
+require(dplyr)
+require(GGally)
+require(reshape2)
+library(caret)
 library(SparkR, lib.loc = "/usr/local/spark/R/lib")
 
 sc <- sparkR.init(master = "", appName = "SparkR",
             sparkHome = "/usr/local/spark", sparkEnvir = list(),
             sparkExecutorEnv = list(), sparkJars = "", sparkPackages = "")
 sqlContext <- sparkRSQL.init(sc)
-people <- read.df(sqlContext, "workspaceR/predictionwithBigData/data/dados_2.json", "json")
+people <- read.df(sqlContext, "data/dados_2.json", "json")
 #
 people <- withColumn(people, "DIST", people$LON*0)
 people <- withColumn(people, "TIME", people$LON*0)
 people <- SparkR::arrange(people, asc(people$COD_LINHA), asc(people$VEIC), asc(people$DTHR))
-
-
-
 aux <-collect(people)
 
 aux <-transform(aux,COD_LINHA = as.integer(COD_LINHA),DTHR = as.POSIXct(DTHR, format = "%d/%m/%Y %H:%M:%S"), DIST = as.numeric(DIST), TIME = as.numeric(TIME), LAT = as.numeric(gsub( ",", ".", aux$LAT)), LON = as.numeric(gsub( ",", ".", aux$LON)))
-
-
-
 aux<-setDistAndTime()
 
-summary(aux)
+linha222 <- dplyr::filter(aux, aux$COD_LINHA=="222")
+ggpairs(linha222)
+df <- melt(linha222)
+trainIndex <- createDataPartition(linha222$TIME, p = .75, list = FALSE)
+train <- linha222[trainIndex,]
+test <- linha222[-trainIndex,]
+str(train)
+model <- glm(TIME ~ ., data = train, family="gaussian")
+summary(model, signif.stars=TRUE)
+preds <- SparkR::predict(model, dplyr::select(test,DIST))
+residuals <- collect(agg(preds,SS_res=sum(preds$)))
 
-
+linha812 <- filter(aux, aux$COD_LINHA=="812")
+linha822 <- filter(aux, aux$COD_LINHA=="822")
+linha827 <- filter(aux, aux$COD_LINHA=="827")
 dist_pontos <- function(x1,y1,x2,y2){
   return(sqrt((((x2-x1)**2)+((y2-y1)**2))))
 }
